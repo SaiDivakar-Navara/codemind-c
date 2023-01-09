9495d9df887bde3507c28090dd0e391d5d78bd16
SaiDivakar-Navara <119742639+SaiDivakar-Navara@users.noreply.github.com>
2023-01-09 19:41:14 +0530

    #include<stdio.h> int main() {     int n,c,a,x;     scanf("%d",&n);     c=n/10;     a=n%10;     if(a%5==0)     {         x=c+(a/5);         printf("%d",x);     }     else     {     printf("-1");     }
    }

---
+++ Kohli_and_Coins.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,c,a,x;
+    scanf("%d",&n);
+    c=n/10;
+    a=n%10;
+    if(a%5==0)
+    {
+        x=c+(a/5);
+        printf("%d",x);
+    }
+    else
+    {
+    printf("-1");
+    }
+    
+}
