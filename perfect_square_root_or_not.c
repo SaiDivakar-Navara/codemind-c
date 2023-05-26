3c43d99c6248c457b01f5c8f49f603bb285eefc7
SaiDivakar-Navara <119742639+SaiDivakar-Navara@users.noreply.github.com>
2023-05-26 14:45:23 +0530

    #include <stdio.h> int isPerfectSquare(int num) {     int i;     for (i = 1; i * i <= num; i++) {         if (i * i == num)             return 1;     }     return 0; } int main() {     int num;     scanf("%d", &num);
        if (isPerfectSquare(num))
            printf("True");
        else
            printf("False");
        
        return 0;
    }

--- perfect_square_root_or_not.c
+++ perfect_square_root_or_not.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
-#include <math.h>
-int main()
-{
+int isPerfectSquare(int num) {
+    int i;
+    for (i = 1; i * i <= num; i++) {
+        if (i * i == num)
+            return 1;  
+    }
+    return 0;  
+}
+int main() {
     int num;
-    int v;
-    float f;
-    scanf("%d",&num);
-    f=sqrt((double)num);
-    v=f;
-    if(v==f)
-        printf("True",num);
+    scanf("%d", &num);
+    
+    if (isPerfectSquare(num))
+        printf("True");
     else
-        printf("False",num);
+        printf("False");
+    
+    return 0;
 }
