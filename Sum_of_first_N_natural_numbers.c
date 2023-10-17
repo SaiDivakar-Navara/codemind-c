491fe3c0f4c3483e87a3a7fc9b4d70a89e14568c
SaiDivakar-Navara <119742639+SaiDivakar-Navara@users.noreply.github.com>
2023-10-17 09:55:10 +0530

    #include<stdio.h> int main(){     int n,sum=0;     scanf("%d",&n);     for(int i=1;i<n+1;i++){         sum=sum+i;     }     printf("%d",sum); }

--- Sum_of_first_N_natural_numbers.c
+++ Sum_of_first_N_natural_numbers.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
-int main()
-{
-    int a,i,r;
-    scanf("%d",&a);
-    for(r=0,i=1;i<=a;i++)
-    {
-        r=r+i;
+int main(){
+    int n,sum=0;
+    scanf("%d",&n);
+    for(int i=1;i<n+1;i++){
+        sum=sum+i;
     }
-    printf("%d",r);
+    printf("%d",sum);
 }
